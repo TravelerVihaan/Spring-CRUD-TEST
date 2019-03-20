4206946fc09c1828a9b7d15cc00611d9b67b3b7f
Vihaan <32998032+TravelerVihaan@users.noreply.github.com>
2019-03-20 12:56:27 +0100

    Add files via upload

--- Spring-TEST-app/src/main/java/pl/javastart/controller/ListController.java
+++ Spring-TEST-app/src/main/java/pl/javastart/controller/ListController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pl.javastart.model.Person;
 import pl.javastart.repositotory.PersonRepository;
@@ -23,10 +25,10 @@ public class ListController {
 	
 	@GetMapping("/show")
 	public String fullList(Model model) {
-		List<Person> list = new ArrayList<Person>();
-		list = (List<Person>) personRepo.findAll();
-		model.addAttribute("list",list);
-		return "showall";
+			List<Person> list = new ArrayList<Person>();
+			list = (List<Person>) personRepo.findAll();
+			model.addAttribute("list",list);
+			return "showall";
 	}
-	
 }
+	
